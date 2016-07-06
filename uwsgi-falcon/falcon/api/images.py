cf6de81eabff0cbac54ba777dabeffae4274fa22
Nadav <nadav@trendiguru.com>
2016-07-06 15:18:45 +0300

    method

--- uwsgi-falcon/falcon/api/images.py
+++ uwsgi-falcon/falcon/api/images.py
@@ -15,29 +15,39 @@ class Resource(object):
                     'Access-Control-Max-Age': '86400'}
 
     def on_get(self, req, resp):
-        # page_url = req.get_param("pageUrl")
-        # image_url = req.get_param("imageUrl")
-        # exists = req.get_param("exists")
-        # relevant = req.get_param("relevant")
-        # lang = req.get_param("lang")
-        #
-        # if lang:
-        #     page_results.set_lang(lang)
-        #
-        # if 'fashionseoul' in image_url:
-        #     products = "GangnhamStyle"
-        # else:
-        #     products = "ShopStyle"
-        # # products = determine with pageUrl
-        #
-        # ret = page_results.get_data_for_specific_image(image_url=image_url, products_collection=products)
+        ret = {}
+        page_url = req.get_param("pageUrl")
+        image_url = req.get_param("imageUrl")
+        exists = req.get_param("exists")
+        relevant = req.get_param("relevant")
+        lang = req.get_param("lang")
 
-        ret = {"hello": "world!", 
-                "collection_names": constants.db.collection_names()}
+        if 'fashionseoul' in image_url:
+            products = "GangnamStyle"
+        else:
+            products = "ShopStyle"
+
+        if lang:
+            page_results.set_lang(lang)
+
+        if page_url:
+            ret = page_results.search_existing_images(page_url)
+        elif image_url:
+            if exists:
+                ret = {"exists": page_results.image_exists(image_url)}
+            elif relevant:
+                ret = {"relevant": page_results.is_image_relevant(image_url)}
+            else:
+                ret = page_results.get_data_for_specific_image(image_url=image_url, products_collection=products)
+        else:
+            resp.status = falcon.HTTP_400
+
+        # ret = {"hello": "world!",
+        #         "collection_names": constants.db.collection_names()}
 
         resp.data = json_util.dumps(ret) + "\n"
         resp.content_type = 'application/json'
-        resp.set_headers(cors_headers)
+        resp.set_headers(self.cors_headers)
         resp.status = falcon.HTTP_200
 
     def on_post(self, req, resp):
@@ -46,19 +56,12 @@ class Resource(object):
             data = json_util.loads(req.stream.read())
             page_url = data.get("pageUrl")
             images = data.get("imageList")
-            lang = data.get("lang")
-            coll_name = page_results.set_lang(lang)
+            if 'method' in req.params:
+                method = req.params["method"]
+            else:
+                method = 'pd'
             if type(images) is list and page_url is not None:
-                relevancy_dict = {}
-                new_images = []
-                for url in images:
-                    if page_results.is_image_relevant(url, coll_name):
-                        relevancy_dict[url] = True
-                    else:
-                        new_images.append(url)
-                        relevancy_dict[url] = False
-                    [q1.enqueue_call(func='fiction.function', args=(page_url, image_url, lang), ttl=2000,
-                                     result_ttl=2000, timeout=2000) for image_url in new_images]
+                relevancy_dict = {url: page_results.route_by_url(url, page_url, method) for url in images}
                 ret["success"] = True
                 ret["relevancy_dict"] = relevancy_dict
             else:
@@ -70,5 +73,5 @@ class Resource(object):
         
         resp.data = json_util.dumps(ret)
         resp.content_type = 'application/json'
-        resp.set_headers(cors_headers)
+        resp.set_headers(self.cors_headers)
         resp.status = falcon.HTTP_200
