28a5da746bdb78c3f18ec4aa5a986b1094fb00d7
jeremy <jeremy@trendiguru.com>
2016-07-20 21:22:41 +0300

    cudnn runtime instead of devel

--- docker_login_gcr.sh
+++ docker_login_gcr.sh
@@ -14,11 +14,11 @@ echo "​docker run -v $(pwd):/tmp -t -i --name gcloud-config google/cloud-sdk g
 docker run -v $(pwd):/tmp -t -i --name gcloud-config google/cloud-sdk gcloud auth activate-service-account $email --key-file /tmp/$keyfile --project $project_id
 
 
-token=$(docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk gcloud auth print-access-token)
+token="$(docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk gcloud auth print-access-token)"
 echo $token
 
 echo "docker login -e $email -u _token -p $token https://$gcr_url"
-docker login -e $email -u _token -p $token https://$gcr_url
+docker login  $email -u oauth2accesstoken -p $token https://$gcr_url
 
 docker pull eu.gcr.io/test-paper-doll/tg/base:1
 
