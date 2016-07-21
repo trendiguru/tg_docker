561f4fba0cd1baf4d4d109a6c2824f2e77d291d5
jeremy <jeremy@trendiguru.com>
2016-07-21 10:41:05 +0300

    cudnn runtime instead of devel

--- docker_login_json.sh
+++ docker_login_json.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-docker login -u _json_key -p "$(cat test-paper-doll-0e7488b90747.json)" https://$gcr_url
+project_id='test-paper-doll'
+gcr_url='eu.gcr.io'
+json_file='test-paper-doll-0e7488b90747.json'
+docker login -u _json_key -p "$(cat $json_file)" https://$gcr_url
 
 #docker pull eu.gcr.io/test-paper-doll/tg/base:1
